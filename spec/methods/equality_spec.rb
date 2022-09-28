7d727b0c4035d8f945174115c57fdfffac1e5da0
nautilus@octopus <nautilus@trovaprezzi.it>
2022-09-28 17:31:17 +0200

    prove.

---
+++ spec/methods/equality_spec.rb
@@ -0,0 +1,54 @@
+require_relative('../../models/person.rb')
+
+class A; end
+class B < A; end
+
+describe('Equality') do
+
+  it 'equal? (based on .object_id method)' do
+    a, b = 'xyz', 'xyz'
+    expect(a.equal?(a)).to be_truthy
+    expect(a.object_id == a.object_id).to be_truthy
+
+    expect(a.equal?(b)).to be_falsey
+    expect(a.object_id != b.object_id).to be_truthy
+  end
+
+  it 'eql? (based on .hash method) (check type of class)' do
+    a, b = 'xyz', 'xyz'
+    expect(a.eql?(b)).to be_truthy # stesso valore e stessa classe
+    expect(a.hash == b.hash).to be_truthy
+    expect(a.class == b.class).to be_truthy
+
+    a, b = 1, 1.0
+    expect(a.eql?(b)).to be_falsey # stesso valore ma classe diversa
+    expect(a.hash == b.hash).to be_falsey
+    expect(a.class == b.class).to be_falsey
+  end
+
+  it '== (based on .hash method) (no check type of class) (same as === but used for if statements)' do
+    a, b = 'xyz', 'xyz'
+    expect(a == b).to be_truthy # stesso valore e stessa classe
+    expect(a.hash == b.hash).to be_truthy
+    expect(a.class == b.class).to be_truthy
+
+    a, b = 1, 1.0
+    expect(a == b).to be_truthy # stesso valore, classe diversa, ma non facendo il check sulla classe va bene
+    expect(a.hash == b.hash).to be_falsey
+    expect(a.class == b.class).to be_falsey
+  end
+
+  it '=== (based on .hash method) (no check type of class) (same as == but used for case statements)' do
+    a, b = 'xyz', 'xyz'
+    expect(a === b).to be_truthy
+    expect(a.hash == b.hash).to be_truthy
+    expect(a.class == b.class).to be_truthy
+
+    a, b = 1, 1.0
+    expect(a === b).to be_truthy
+    expect(a.hash == b.hash).to be_falsey
+    expect(a.class == b.class).to be_falsey
+  end
+
+end
+
